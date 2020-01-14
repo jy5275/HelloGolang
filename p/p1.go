757af08748b4ec591e71ea6b6ada4b9b6e575857
jy5275 <527507046@qq.com>
2020-01-15 01:38:42 +0800

    Add p2.go

--- p/p1.go
+++ p/p1.go
@@ -2,6 +2,12 @@ package p
 
 import "fmt"
 
+// PVar ...
+var PVar int = 10
+
+// str ...
+var str string = "Hello, world!"
+
 // Init1 ...
 func init() {
 	fmt.Println("Initializing function in p1.go ...")
---
+++ p/p2.go
@@ -0,0 +1,7 @@
+package p
+import "fmt"
+func init(){
+	fmt.Println("init in p2.go begin")
+	fmt.Println(str)
+	fmt.Println("init in p2.go end")
+}
