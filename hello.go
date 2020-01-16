61c125200baeeee131f97418a838fecb4bd156c6
jiangyan.x <jiangyan.x@bytedance.com>
2020-01-16 14:30:24 +0800

    hello.go renewed

--- hello.go
+++ hello.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/jy5275/hello/morestrings"
-	_ "github.com/jy5275/hello/p"
+	"github.com/jy5275/HelloGolang/morestrings"
+	_ "github.com/jy5275/HelloGolang/p"
 )
 
 func main() {
 	//p.Init1()
-	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(morestrings.ReverseRunes("Reversed hello"))
 	fmt.Println(cmp.Diff("Hello, World", "Hello, Go"))
 }
