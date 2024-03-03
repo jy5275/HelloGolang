889729ca22287d855e5ba91d9894068b34e5df9a
Yan Jiang <yan.jiang@aiprm.org>
2024-03-03 22:30:17 +0700

    gmp learning

--- hello.go
+++ hello.go
@@ -7,24 +7,30 @@ import (
 	"fmt"
 	_ "net/http/pprof"
 	"runtime"
+	"sync"
+	"time"
 )
 
-func main() {
-	//adv.NetWorkTest()
-	//funcObj()
-	//stringTest()
-	//NewMakeTest()
-	deferTest()
-	//adv.MarshalTest()
-	//adv.UnmarshalTest()
-	//ifTest()
-	//sTest()
-
-	// Goroutine pratice
-	//adv.FibTest()
-	//adv.CalcMain()
-	//adv.CtxMain()
-	//adv.SquareAndCube(589)
-	//adv.PCMain()
+func GMP() {
 	fmt.Println(runtime.NumGoroutine())
+	fmt.Println(runtime.NumCPU())
+	//debug.SetMaxThreads(10)
+	var wg sync.WaitGroup
+	for i := 0; i < 2000; i++ {
+		wg.Add(1)
+		go func() {
+			a := 0
+			for i := 0; i < 1e6; i++ {
+				a += 1
+			}
+			wg.Done()
+		}()
+		time.Sleep(100 * time.Millisecond)
+	}
+	wg.Wait()
+}
+
+func main() {
+	GMP()
+
 }
